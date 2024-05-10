b39223bd46a2729a2d805be884d8f291508663ab
LuscaAnasta <lucasanastacio140@gmail.com>
2024-05-10 02:25:59 -0300

    Desafio POO DIO

---
+++ DesafioPOO/src/edu/LuscaAnasta/smartphone/aplicativos/Aparelho_Telefonico.java
@@ -0,0 +1,7 @@
+package edu.LuscaAnasta.smartphone.aplicativos;
+
+public interface Aparelho_Telefonico {
+	public void ligar();
+	public void atender();
+	public void correioDeVoz();
+}
---
+++ DesafioPOO/src/edu/LuscaAnasta/smartphone/aplicativos/Navegador_Internet.java
@@ -0,0 +1,7 @@
+package edu.LuscaAnasta.smartphone.aplicativos;
+
+public interface Navegador_Internet {
+	public void exibirPagina();
+	public void adicionarNovaAba();
+	public void atualizarPagina();
+}
